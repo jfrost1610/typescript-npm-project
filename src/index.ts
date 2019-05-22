9bcf499102b419c533bd0e6bf02908353ea5d876
jfrost1610 <jobinthomas1610@gmail.com>
2019-05-22 09:05:49 +0530

    Updated project structure

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { GithubApiService } from './GithubApiService';
+import { User } from './User';
+import { Repo } from './Repo';
+import * as _ from 'lodash';
+
+let svc = new GithubApiService();
+
+if (process.argv.length < 3) {
+    console.error("Please pass the username as an argument!");
+} else {
+
+    let userName: string = process.argv[2];
+
+    svc.getUserInfo(userName, (user: User) => {
+        svc.getRepos(userName, (repos: Repo[]) => {
+            let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.size * (-1)]);
+            let filteredRepos = _.take(sortedRepos, 5);
+            user.repos = filteredRepos;
+            console.log(user);
+        });
+    });
+}
+
